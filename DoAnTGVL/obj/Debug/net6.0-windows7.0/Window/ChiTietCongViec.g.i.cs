bc77cc205f653b762c1c1121fb2627b76d3024f5
Danhuynh224 <vdan22422004@gmail.com>
2024-05-10 13:48:26 +0700

    done hong

--- DoAnTGVL/obj/Debug/net6.0-windows7.0/Window/ChiTietCongViec.g.i.cs
+++ DoAnTGVL/obj/Debug/net6.0-windows7.0/Window/ChiTietCongViec.g.i.cs
@@ -61,7 +61,7 @@ namespace DoAnTGVL {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DoAnTGVL;component/window/chitietcongviec.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DoAnTGVL;V1.0.0.0;component/window/chitietcongviec.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\WINDOW\ChiTietCongViec.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- DoAnTGVL/obj/Debug/net6.0-windows7.0/Window/ChinhSuaBaiDang.g.i.cs
+++ DoAnTGVL/obj/Debug/net6.0-windows7.0/Window/ChinhSuaBaiDang.g.i.cs
@@ -78,7 +78,7 @@ namespace DoAnTGVL {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DoAnTGVL;component/window/chinhsuabaidang.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DoAnTGVL;V1.0.0.0;component/window/chinhsuabaidang.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\WINDOW\ChinhSuaBaiDang.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
